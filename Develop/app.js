9185b63ae4368f2e554f971ac651281fa1c63f91
Kevin <millhousekevin@gmail.com>
2020-08-03 00:09:07 -0400

    fixing typos

--- Develop/app.js
+++ Develop/app.js
@@ -12,73 +12,33 @@ const render = require("./lib/htmlRenderer");
 
 const teamMembers = [];
 
-
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-function promptUser() {
+function promptManager() {
+  console.log("enter manager info!");
   return inquirer.prompt([
-    { type: "confirm",
-      name: "validate",
-      message:"are you manager?"
-
-    },
     {
       type: "input",
       name: "name",
-      message: "What is your name?"
+      message: "What is your name?",
     },
     {
       type: "input",
       name: "id",
-      message: "What is your id?"
+      message: "What is your id?",
     },
     {
       type: "input",
       name: "email",
-      message: "What is your email address?"
+      message: "What is your email address?",
     },
     {
       type: "input",
       name: "officeNumber",
-      message:"what is your office number?"
-    },
-    
-  ])
-}
-
-function teamSelect(){
-  return inquirer.prompt([{
-    type:"list",
-    name:"teamMembers",
-    message:"Would you like to add a team?",
-    choices:[
-      "engineer",
-      "intern",
-      "NO"
-    ]
-  }]).then(function(choice){
-    
-    switch(choice.teamMembers){
-      case "engineer":
-        engQuestion().then(function(egdata){
-  
-          const engineer = new Engineer(answers.name, answers.id, answers.email, egdata.github);
-          teamMembers.push(engineer);
-          console.log(teamMembers);
-          
-      }); break 
-      case "intern": 
-      
-     intQuestion().then(function(indata){
-  
-        const intern = new Intern(answers.name, answers.id, answers.email, indata.officeNumber);
-        teamMembers.push(intern);
-    });break; 
+      message: "what is your office number?",
     }
-
-  })
+  ]);
 }
 
 function engQuestion() {
@@ -86,24 +46,24 @@ function engQuestion() {
     {
       type: "input",
       name: "name",
-      message: "What is your name?"
+      message: "What is your engineer's name?",
     },
     {
       type: "input",
       name: "id",
-      message: "What is your id?"
+      message: "What is your engineer's id?",
     },
     {
       type: "input",
       name: "email",
-      message: "What is your email address?"
-    },{
+      message: "What is your engineer's email address?",
+    },
+    {
       type: "input",
       name: "github",
-      message:"what is your github user name?"
+      message: "what is your engineer's github user name?",
     }
-  ])
-  
+  ]);
 }
 
 function intQuestion() {
@@ -111,90 +71,89 @@ function intQuestion() {
     {
       type: "input",
       name: "name",
-      message: "What is your name?"
+      message: "What is your intern's name?",
     },
     {
       type: "input",
       name: "id",
-      message: "What is your id?"
+      message: "What is your intern's id?",
     },
     {
       type: "input",
       name: "email",
-      message: "What is your email address?"
+      message: "What is your intern's email address?",
     },
     {
       type: "input",
       name: "school",
-      message:"what is the name of your school?"
+      message: "what is the name of your intern's school?",
     }
-  ])
- 
+  ]);
 }
-promptUser().then(function(answers){
 
-  switch(answers.validate){
-    case true: this.role = "manager"
-    const manager = new Manager(answers.name, answers.id, answers.email, role, answers.officeNumber)
-    teamMembers.push(manager);
-    teamSelect();
-  }
-  teamSelect().then(function(choice){
+function teamSelect() {
+  return inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "teamMembers",
+        message: "Would you like to add a team?",
+        choices: ["engineer", "intern", "NO"]
+      }
+    ])
+    .then(function (choice) {
+      switch (choice.teamMembers) {
+        case "engineer":
+          engQuestion().then(function (answers) {
+            const engineer = new Engineer(
+              answers.name,
+              answers.id,
+              answers.email,
+              answers.github
+            );
+            teamMembers.push(engineer);
+            console.log(teamMembers);
+            teamSelect();
+          });
+          break;
 
-    switch (choice.teamMembers){
-   
-    case "engineer": 
-      
-     engQuestion().then(function(egdata){
+        case "intern":
+          intQuestion().then(function (answers) {
+            const intern = new Intern(
+              answers.name,
+              answers.id,
+              answers.email,
+              answers.school
+            );
+            teamMembers.push(intern);
+            console.log(teamMembers);
+            teamSelect();
+          });
+          break;
+        case "NO":
+          finalize();
+          break;
+      }
+    });
+}
+
+promptManager().then(function (answers) {
   
-        const engineer = new Engineer(answers.name, answers.id, answers.email, egdata.github);
-        teamMembers.push(engineer);
-        console.log(teamMembers);
-        
-    }); break 
-    case "intern": 
-      
-     intQuestion().then(function(indata){
+      this.role = "manager";
+      const manager = new Manager(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.officeNumber
+      );
+      teamMembers.push(manager);
+      teamSelect();
   
-        const intern = new Intern(answers.name, answers.id, answers.email, indata.officeNumber);
-        teamMembers.push(intern);
-    });break;
-    case "NO": 
-      
-    intQuestion().then(function(indata){
- 
-       const intern = new Intern(answers.name, answers.id, answers.email, indata.officeNumber);
-       console.log(intern);
-   });break;
-  }
-  
-  })
 });
 
-
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// fs.writeFile(outputPath, data, (err)=>{
-//   if(err)
-//     throw err;
-//     console.log(err)
-  
-// });
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+function finalize(){
+fs.writeFile(outputPath, render(teamMembers), (err)=>{
+  if(err)
+    throw err;
+});
+}
